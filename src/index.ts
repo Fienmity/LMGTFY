81132ac8f7ad50f6ce0ac4bd7db51f50497687a4
Jack Matthews <jm5112356@gmail.com>
2022-05-05 18:28:54 -0400

    Initial LMGTFY

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,61 @@
+import { ApplicationCommandInputType, ApplicationCommandOptionType, ApplicationCommandType, Command, EnmitySectionID } from "enmity-api/commands";
 import { Plugin, registerPlugin } from "enmity-api/plugins";
 
-const ExamplePlugin: Plugin = {
-  name: "ExamplePlugin",
+const LMGTFY: Plugin = {
+  name: "LMGTFY",
+  // @ts-ignore
+  author: 'Fiery',
+  version: '1.0.0',
+  description: "Let me google that for you...",
+  color: "#4285F4",
+  authors: [
+    {
+      name: 'Fiery',
+      id: '890228870559698955'
+    }
+  ],
   commands: [],
-  patches: [],
 
   onStart() {
+    const lmgtfyCommand: Command = {
+      id: "lmgtfy-command",
+      applicationId: EnmitySectionID,
 
+      name: "lmgtfy",
+      displayName: "lmgtfy",
+
+      description: "Let me google that for you...",
+      displayDescription: "Let me google that for you...",
+
+      type: ApplicationCommandType.Chat,
+      inputType: ApplicationCommandInputType.BuiltInText,
+
+      options: [{
+        name: "search",
+        displayName: "search",
+
+        description: "What they want to google",
+        displayDescription: "What they want to google",
+
+        type: ApplicationCommandOptionType.String,
+        required: true
+      }],
+      
+      execute: function (args, _) {
+        const search = args[0].value;
+
+        return {
+          content: `<https://letmegooglethat.com/?q=${encodeURIComponent(search)}>`
+        }
+      }
+    }
+
+    this.commands.push(lmgtfyCommand)
   },
 
   onStop() {
-
+    this.commands = []
   }
 }
 
-registerPlugin(ExamplePlugin);
+registerPlugin(LMGTFY);
